ae3905da63e38849f5c82c40da6638c1e844b24c
Anjelica <ambrosio.anjelica@gmail.com>
2020-04-08 12:48:06 -0700

    pasted last bit from google doc

--- problem1.py
+++ problem1.py
@@ -2,6 +2,11 @@
 List: (1, 2, 3, 4, 4, 5)
 Set = {1, 2, 3, 4, 4, 5} #< --- I turned the list into a set
 #Another way would be Set(list).
+# Given a list of an unspecified number of numbers, determine if the list has a duplicate number.
+#
+# Questions:
+# How many numbers are there?
+# Are the numbers integers or strings?
 
 def find_the_dup(list, set):
 “””Check if list has duplicates by comparing to set”””
