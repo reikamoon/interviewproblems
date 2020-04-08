db0ff4c986f024860bcdc280144030451bd676da
Anjelica <ambrosio.anjelica@gmail.com>
2020-04-08 13:32:26 -0700

    added final comments

--- problem1.py
+++ problem1.py
@@ -2,6 +2,7 @@
 List = [1, 2, 3, 4, 4, 5]
 Set = {1, 2, 3, 4, 4, 5} #< --- I turned the list into a set
 #Another way would be Set(list).
+
 # Given a list of an unspecified number of numbers, determine if the list has a duplicate number.
 #
 # Questions:
@@ -15,6 +16,8 @@ def find_the_dup(list):
         return True
 
 # Time Complexity: O(1), because it only goes through each element once.
+# What does this function do? It turns lists into sets, and as sets already leave out duplicates, it
+#checks if the set is smaller than the list. If the set is smaller, there's a duplicate!
 
 if __name__ == '__main__':
     list = [1, 2, 3, 4, 4, 5]
