9b764841006bd217751124ec7bd0f5cc7c75a687
Anjelica <ambrosio.anjelica@gmail.com>
2020-04-08 13:30:12 -0700

    added edits, function works properly

--- problem1.py
+++ problem1.py
@@ -1,5 +1,5 @@
 #Data Structure I will use: Set
-List: (1, 2, 3, 4, 4, 5)
+List = [1, 2, 3, 4, 4, 5]
 Set = {1, 2, 3, 4, 4, 5} #< --- I turned the list into a set
 #Another way would be Set(list).
 # Given a list of an unspecified number of numbers, determine if the list has a duplicate number.
@@ -8,11 +8,19 @@ Set = {1, 2, 3, 4, 4, 5} #< --- I turned the list into a set
 # How many numbers are there?
 # Are the numbers integers or strings?
 
-def find_the_dup(list, set):
-“””Check if list has duplicates by comparing to set”””
-If len(list) === len(set): #Another way would be if len(list) == len(set(list)):
-Print (“Duplicate found!”)
-Return true
-Else
-print(“No Duplicates found :L”)
-Return false
+def find_the_dup(list):
+    if len(list) == len(set(list)):
+        return False
+    else:
+        return True
+
+# Time Complexity: O(1), because it only goes through each element once.
+
+if __name__ == '__main__':
+    list = [1, 2, 3, 4, 4, 5]
+    result = find_the_dup(list)
+
+    if result:
+        print('List does contain duplicates.')
+    else:
+        print("No duplicates found in the list.")
